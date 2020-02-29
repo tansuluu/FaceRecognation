e2ad7f72bfac586c24c0be849a7e57f0080d96e5
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2020-03-01 02:18:27 +0600

    Registration

--- src/main/java/aiu/edu/kg/FaceRecognation/controller/UserController.java
+++ src/main/java/aiu/edu/kg/FaceRecognation/controller/UserController.java
@@ -43,12 +43,10 @@ public class UserController {
 
         return "redirect:/welcome";
     }
-
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
-
         if (logout != null)
             model.addAttribute("message", "You have been logged out successfully.");
 
