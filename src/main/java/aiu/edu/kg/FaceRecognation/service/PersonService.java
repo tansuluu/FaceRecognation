0cde1bbe26964ce70de3827f9c4ab8c5d39ddbb1
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2020-05-05 01:17:06 +0600

    Person controller added

--- src/main/java/aiu/edu/kg/FaceRecognation/service/PersonService.java
+++ src/main/java/aiu/edu/kg/FaceRecognation/service/PersonService.java
@@ -1,13 +1,20 @@
 package aiu.edu.kg.FaceRecognation.service;
 
+import aiu.edu.kg.FaceRecognation.entity.Person;
 import aiu.edu.kg.FaceRecognation.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PersonService {
 
     @Autowired
     private PersonRepository personRepository;
+
+    public List<Person> findAll(){
+        return personRepository.findAll();
+    }
 
 }
--- src/main/java/aiu/edu/kg/FaceRecognation/service/SecurityService.java
+++ src/main/java/aiu/edu/kg/FaceRecognation/service/SecurityService.java
@@ -1,6 +1,7 @@
 package aiu.edu.kg.FaceRecognation.service;
 
 public interface SecurityService {
+
     String findLoggedInUsername();
 
     void autoLogin(String username, String password);
