6fba4e3470b0cbc68f252a5f0cbbb7da317058c4
tansuluu <tansuluu.myrzaeva@iaau.edu.kg>
2020-05-17 19:48:10 +0600

    small sql fix

--- python project/app/repository/native_query.py
+++ python project/app/repository/native_query.py
@@ -1,13 +1,13 @@
 def update_request(status, id):
     from app import db
-    query = 'update REQUEST set status = %s  where id = %s'
+    query = 'update request set status = %s  where id = %s'
     db.engine.execute(query, (status,  id))
     db.session.close()
 
 
 def get_request():
     from app import db
-    query = 'SELECT * FROM REQUEST where status="NEW" order by created_date desc limit 1'
+    query = 'SELECT * FROM request where status="NEW" order by created_date desc limit 1'
     result = db.engine.execute(query).fetchall()
     db.session.close()
     return result
@@ -15,7 +15,7 @@ def get_request():
 
 def save_request_result(file_name, process_id, percentage, person_id):
     from app import db
-    query = 'insert into request_result(CREATED_DATE, file_name, request_process_id, percentage, person_id) ' \
+    query = 'insert into request_result(created_date, file_name, request_process_id, percentage, person_id) ' \
             'values(current_date ,%s, %s, %s, %s)'
     db.engine.execute(query, (file_name, process_id, str(percentage), person_id))
     db.session.close()
@@ -23,7 +23,7 @@ def save_request_result(file_name, process_id, percentage, person_id):
 
 def get_people_to_process():
     from app import db
-    query = 'SELECT * FROM PERSON where face_encodings is null order by created_date desc limit 1'
+    query = 'SELECT * FROM person where face_encodings is null and removed_date is null order by created_date desc limit 1'
     result = db.engine.execute(query).fetchall()
     db.session.close()
     return result
@@ -33,7 +33,7 @@ def update_person(face_encodings, id):
     import pickle
     from app import db
     face_pickled_data = pickle.dumps(face_encodings)
-    query = 'update PERSON set face_encodings = %s  where id = %s'
+    query = 'update person set face_encodings = %s  where id = %s'
     db.engine.execute(query, (face_pickled_data,  id))
     db.session.close()
 
@@ -57,16 +57,16 @@ def get_request_process(request_id):
 def get_people(request):
     from app import db
     if request.gender is not None and request.person_position is not None:
-        query = 'SELECT * FROM PERSON where face_encodings is not null and removed_date is null and gender=%s and person_position=%s order by created_date desc'
+        query = 'SELECT * FROM person where face_encodings is not null and removed_date is null and gender=%s and person_position=%s order by created_date desc'
         result = db.engine.execute(query, (request.gender, request.person_position)).fetchall()
     elif request.gender is not None and request.person_position is None:
-        query = 'SELECT * FROM PERSON where face_encodings is not null and removed_date is null and gender=%s order by created_date desc'
+        query = 'SELECT * FROM person where face_encodings is not null and removed_date is null and gender=%s order by created_date desc'
         result = db.engine.execute(query, (request.gender)).fetchall()
     elif request.gender is None and request.person_position is not None:
-        query = 'SELECT * FROM PERSON where face_encodings is not null and removed_date is null and person_position=%s order by created_date desc'
+        query = 'SELECT * FROM person where face_encodings is not null and removed_date is null and person_position=%s order by created_date desc'
         result = db.engine.execute(query, (request.person_position)).fetchall()
     else:
-        query = 'SELECT * FROM PERSON where face_encodings is not null and removed_date is null order by created_date desc'
+        query = 'SELECT * FROM person where face_encodings is not null and removed_date is null order by created_date desc'
         result = db.engine.execute(query).fetchall()
     db.session.close()
     return result
